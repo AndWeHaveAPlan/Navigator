4f3e84f7626cdbfe6980d693276247e4d233a5c3
Евгений Снитко <esnitko@click2.money>
2021-07-26 19:03:01 +0300

    playground

---
+++ Navigator.Playground/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Navigator.Core;
+
+namespace Navigator.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host =
+                ImmateriumHost.CreateDefaultBuilder()
+                    .UseSerializer()
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
